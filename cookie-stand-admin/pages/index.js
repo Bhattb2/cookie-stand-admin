4aaa5199cdd26d74e99e3ad7f893108d588da2ca
Bhagirath Bhatt <budbhatt@gmail.com>
2021-04-08 17:19:20 -0700

    still work in progress

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -1,6 +1,22 @@
 import Head from 'next/head'
+import React, { useState } from 'react'
 
 export default function Home() {
+
+  const[location, setLocation] = useState('Location Here')
+  const[minCust, setMinCust] = useState('Min Customers Here')
+  const[maxCust, setMaxCust] = useState('Max Customers Here')
+  const[avgCookies, setAvgCookies] = useState('Average Cookies Sold Here')
+
+  function formHandler(event){
+    event.preventDefault();
+    setLocation(event.target.location.value)
+    setMinCust(event.target.min_cuat.value)
+    setMaxCust(event.target.max_cust.value)
+    setAvgCookies(event.target.avg_per.value)
+  }
+
+
   return (
     <div className="">
       <Head>
@@ -8,18 +24,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <main className="">
-  
-      </main> */}
 
-
-      <header className="bg-green-500 p-4 text-black-50">
+      <header className="flex-1 text-2xl text-left bg-green-500 p-4">
         <h1>Cookie Stand Admin</h1>
-
       </header>
 
-      <main className="">
-      <div className="my-6 p-1 mx-20 bg-green-200 rounded-lg w-100 h-40">
+      <main className="m-8">
+      <form onSubmit={formHandler} className=" grid gap-1 bg-green-200 rounded grid-cols-8 mb-4">
+        <legend className='col-start-4 col-span-2 text-2xl m-2'>Create Cookie Stand</legend>
+        <label className='row-start-2 col-start-1 col-span-1 ml-8' for='location'>Location</label>
+        <input className='row-start-2 col-start-2 col-span-7 text-sm mr-4' name='location'></input>
+        <label className='row-start-3 col-start-1 col-span-2 text-sm text-center' for='min_cust'>Minimum Customers Per Hour</label>
+
+
         <div className="">
           <p className="text-center" >Create Cookie Stand</p>
           <form className="flex p3 mx-0">
@@ -42,7 +59,7 @@ export default function Home() {
         </nav>
 
 
-      </div>
+      </form>
       </main>
 
 
